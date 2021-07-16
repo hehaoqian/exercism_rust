42ed87d90c2b6d81f090929518327b40945c85e0
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 00:46:14 +0800

    raindrops done

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut s = String::new();
+    if (n % 3 != 0) && (n % 5 != 0) && (n % 7 != 0) {
+        s += n.to_string().as_str();
+    }
+    if n % 3 == 0 {
+        s += "Pling";
+    }
+    if n % 5 == 0 {
+        s += "Plang";
+    }
+    if n % 7 == 0 {
+        s += "Plong";
+    }
+    s
 }
