c89b335dd7f9132d52a8ef84dfdc25d624d655cc
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 14:24:13 +0800

    bob done

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let msg: String = message.chars().filter(|x| !x.is_whitespace()).collect();
+    if msg.is_empty() {
+        "Fine. Be that way!"
+    } else if msg.chars().any(|x| x.is_alphabetic())
+        && msg
+            .chars()
+            .filter(|x| x.is_alphabetic())
+            .all(|x| x.is_uppercase())
+    {
+        if msg.ends_with('?') {
+            "Calm down, I know what I'm doing!"
+        } else {
+            "Whoa, chill out!"
+        }
+    } else if msg.ends_with('?') {
+        "Sure."
+    } else {
+        "Whatever."
+    }
 }
