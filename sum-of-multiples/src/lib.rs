9d36a86332a63169639f77b9497d5d9ce54cf68f
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 13:18:27 +0800

    sum-of-multiples done

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit).fold(0, |accum, x| {
+        if factors.iter().any(|f| (f != &0) && x % f == 0) {
+            accum + x
+        } else {
+            accum
+        }
+    })
 }
