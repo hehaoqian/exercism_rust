4eb2db6ac267e5c5a1f4b19c78a2c8f965d80851
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 12:56:32 +0800

    proverb done

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut r = String::new();
+    if list.is_empty() {
+        return String::new();
+    }
+    for i in 0..(list.len() - 1) {
+        let word = format!("For want of a {} the {} was lost.\n", list[i], list[i + 1]);
+        r.push_str(&word);
+    }
+    r.push_str(&format!(
+        "And all for the want of a {}.",
+        list.first().unwrap()
+    ));
+    r
 }
