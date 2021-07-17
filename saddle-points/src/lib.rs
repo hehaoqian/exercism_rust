05d7a89fe9c7becb687e8537b75ed4908daf06c6
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 22:34:13 +0800

    saddle-points done

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,15 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut result = Vec::<(usize, usize)>::new();
+
+    let x_max = input.len();
+    let y_max = input[0].len();
+    for x in 0..x_max {
+        for y in 0..y_max {
+            let val = input[x][y];
+            if (0..x_max).all(|r| input[r][y] >= val) && (0..y_max).all(|r| input[x][r] <= val) {
+                result.push((x, y));
+            }
+        }
+    }
+    result
 }
