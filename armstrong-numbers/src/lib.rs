462f1cad553e13e8e0c5c4201dfab2dd1e166876
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 13:57:43 +0800

    armstrong-numbers done

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,8 @@
+use core::f64;
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits = (f64::log10(num as f64) as u32) + 1u32;
+    let armstrong: u32 = (1u32..=digits)
+        .map(|x| (num / 10u32.pow(x - 1u32) % 10u32).pow(digits))
+        .sum();
+    armstrong == num
 }
