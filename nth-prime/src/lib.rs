a0eab8b093036002fc093b1806c186e20411a045
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 11:47:07 +0800

    nth-prime done

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes = Vec::<u32>::new();
+    let mut num = 2;
+    while (primes.len() as u32) <= n {
+        let mut is_prime = true;
+        for prime in &primes {
+            if (num % prime) == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        if is_prime {
+            primes.push(num);
+        }
+        num += 1;
+    }
+    *primes.last().unwrap()
 }
