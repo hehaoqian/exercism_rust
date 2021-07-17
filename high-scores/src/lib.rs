704a23ebca9afe5efc0c4849d2a5de82d52990ba
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 21:52:15 +0800

    high-scores done

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -1,27 +1,38 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        self.scores.last().copied()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        if self.scores.is_empty() {
+            None
+        } else {
+            Some(self.scores.iter().fold(u32::MIN, |accum, x| accum.max(*x)))
+        }
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        let mut v = Vec::from(self.scores);
+        v.sort_by(|a, b| b.cmp(a));
+        let mut r = vec![];
+        for i in 0..3 {
+            if i < v.len() {
+                r.push(v[i]);
+            }
+        }
+        r
     }
 }
