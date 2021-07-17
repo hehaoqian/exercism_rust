a1036552e5a0bb2f8a695d21d7b7e3c089a73c1c
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-17 21:14:09 +0800

    clock done

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,32 @@
-pub struct Clock;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            hours: (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes: minutes.rem_euclid(60),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Self::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
     }
 }
