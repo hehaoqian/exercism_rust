8ce902fbe96117a7884a642fe8e6bc33fb289049
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-18 14:18:51 +0800

    isogram done

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut chars = candidate
+        .to_lowercase()
+        .chars()
+        .filter(|x| x.is_alphabetic())
+        .collect::<Vec<_>>();
+    chars.sort_unstable();
+    for (i, c) in chars.iter().enumerate() {
+        if i > 0 && *c == chars[i - 1] {
+            return false;
+        }
+    }
+    true
 }
