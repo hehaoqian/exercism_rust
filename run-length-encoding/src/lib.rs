32eb3691a54b7c76d0f235ddbf9501219c1316a3
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-18 23:08:07 +0800

    run-length-encoding done

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,7 +1,45 @@
+use std::cmp::max;
+
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut s = String::new();
+    let mut prev_char: Option<char> = None;
+    let mut count = 0;
+    for c in source.chars() {
+        if prev_char == Some(c) {
+            count += 1;
+        } else {
+            if count > 1 {
+                s.push_str(&count.to_string());
+            }
+            if count > 0 {
+                s.push(prev_char.unwrap());
+            }
+            count = 1;
+            prev_char = Some(c);
+        }
+    }
+    if count > 1 {
+        s.push_str(&count.to_string());
+    }
+    if count > 0 {
+        s.push(prev_char.unwrap());
+    }
+    s
 }
 
 pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+    let mut s = String::new();
+    let mut count = 0u32;
+    for c in source.chars() {
+        if c.is_numeric() {
+            count *= 10;
+            count += c.to_digit(10).unwrap();
+        } else {
+            for _ in 0..max(count, 1) {
+                s.push(c);
+            }
+            count = 0;
+        }
+    }
+    s
 }
