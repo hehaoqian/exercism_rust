70197871ae332859f15210b5d7d3e8ade465fee5
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-18 10:02:35 +0800

    anagram done

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let hash_func = |x: &str| {
+        let lower = x.to_lowercase();
+        let mut hash = lower.chars().into_iter().collect::<Vec<_>>();
+        hash.sort_unstable();
+        (lower, hash)
+    };
+    let (lower_word, hash_word) = hash_func(word);
+    possible_anagrams
+        .iter()
+        .filter(|x| {
+            let (lower, hash) = hash_func(x);
+            lower != lower_word && hash == hash_word
+        })
+        .copied()
+        .collect()
 }
