25c1e4e7e7af9894672df065fb2f8c50f70c0c32
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-18 22:41:47 +0800

    triangle done

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,19 +1,33 @@
-pub struct Triangle;
+pub struct Triangle {
+    sides: [u64; 3],
+}
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        unimplemented!("Construct new Triangle from following sides: {:?}. Return None if the sides are invalid.", sides);
+        if sides.iter().any(|x| x <= &0)
+            || (sides[0] + sides[1] <= sides[2])
+            || (sides[1] + sides[2] <= sides[0])
+            || (sides[0] + sides[2] <= sides[1])
+        {
+            None
+        } else {
+            Some(Self { sides })
+        }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        self.sides[0] == self.sides[1] && self.sides[0] == self.sides[2]
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        self.sides[0] != self.sides[1]
+            && self.sides[0] != self.sides[2]
+            && self.sides[1] != self.sides[2]
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        self.sides[0] == self.sides[1]
+            || self.sides[0] == self.sides[2]
+            || self.sides[1] == self.sides[2]
     }
 }
