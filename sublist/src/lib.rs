b4c5175f847bd047d3eba91b66ff0085b0a07238
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-07-19 22:41:12 +0800

    sublist done

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -6,6 +6,36 @@ pub enum Comparison {
     Unequal,
 }
 
+fn is_sublist_strict<T: PartialEq>(short: &[T], long: &[T]) -> bool {
+    if short.len() >= long.len() {
+        return false;
+    }
+    (0..=(long.len() - short.len())).any(|i| &long[i..i + short.len()] == short)
+}
+
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    use std::cmp::Ordering;
+    match _first_list.len().cmp(&_second_list.len()) {
+        Ordering::Equal => {
+            if _first_list == _second_list {
+                Comparison::Equal
+            } else {
+                Comparison::Unequal
+            }
+        }
+        Ordering::Less => {
+            if is_sublist_strict(_first_list, _second_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
+            }
+        }
+        Ordering::Greater => {
+            if is_sublist_strict(_second_list, _first_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
+            }
+        }
+    }
 }
