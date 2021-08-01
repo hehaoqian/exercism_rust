7d23f42ee9fc365f95419c08f587fc1ceee94ff4
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-08-01 10:35:24 +0800

    sieve done

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    let mut marked = Vec::new();
+    marked.resize((upper_bound + 1) as usize, false);
+    marked[0] = true;
+    marked[1] = true;
+    for num in 2..=upper_bound {
+        let mut value = num * 2;
+        while value <= upper_bound {
+            marked[value as usize] = true;
+            value += num;
+        }
+    }
+    marked
+        .into_iter()
+        .enumerate()
+        .filter(|(_, marked)| !*marked)
+        .map(|(num, _)| num as u64)
+        .collect()
 }
