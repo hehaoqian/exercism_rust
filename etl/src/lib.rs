c69f7f681d41ad7235276ea4e4d9390ffdad83d9
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-08-01 09:45:57 +0800

    etl update

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,11 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut r = BTreeMap::new();
-    for (score, chars) in h.iter() {
-        for c in chars.iter() {
-            r.insert(c.to_ascii_lowercase(), *score);
-        }
-    }
-    r
+    h.iter()
+        .flat_map(|(&score, chars)| chars.iter().map(move |c| (c.to_ascii_lowercase(), score)))
+        .collect()
 }
