b8a8081db5d088a17704870b7b0e55c052d815d6
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-08-01 10:14:13 +0800

    acronym done

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,45 @@
+#[derive(PartialEq)]
+enum CharType {
+    Separator,
+    LowerLetter,
+    UpperLetter,
+    Quote,
+}
+
+fn get_char_type(c: char) -> CharType {
+    use CharType::*;
+    match c {
+        '\'' => Quote,
+        _ if c.is_uppercase() => UpperLetter,
+        _ if c.is_lowercase() => LowerLetter,
+        _ => Separator,
+    }
+}
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    use CharType::*;
+    let mut s = String::new();
+
+    if let Some(index) = phrase.find(':') {
+        let prefix = &phrase[..index];
+        if prefix.chars().all(|c| c.is_uppercase()) {
+            return prefix.to_string();
+        }
+    }
+
+    let mut prev_inserted_index = 0usize;
+    let mut prev_char_type = Separator;
+    for (i, c) in phrase.chars().enumerate() {
+        let char_type = get_char_type(c);
+        if char_type != prev_char_type
+            && prev_char_type != Quote
+            && (char_type == LowerLetter || char_type == UpperLetter)
+            && prev_inserted_index + 1 != i
+        {
+            s.push(c.to_ascii_uppercase());
+            prev_inserted_index = i;
+        }
+        prev_char_type = char_type;
+    }
+    s
 }
