0a80a3265181b466441b8111d1083efd2e7c1501
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-08-01 11:31:16 +0800

    binary-search done

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,7 +1,14 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+pub fn find<T: Ord>(array: &[T], key: T) -> Option<usize> {
+    let mut start = 0;
+    let mut end = array.len();
+    while start < end {
+        let mid = (start + end) / 2;
+        use std::cmp::Ordering::*;
+        match array[mid].cmp(&key) {
+            Equal => return Some(mid),
+            Less => start = mid + 1,
+            Greater => end = mid,
+        }
+    }
+    None
 }
