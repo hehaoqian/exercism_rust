a89c7635cecad8e5b410b5c32b219df21796bc94
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-08-05 23:58:17 +0800

    macros done

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,14 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {
-        unimplemented!()
+    (,) => {
+        $compile_error!("Single comma not allowed")
     };
+    ($($key: expr => $value: expr),*$(,)?) => {
+        {
+            use ::std::collections::HashMap;
+            let mut hm = HashMap::new();
+            $(hm.insert($key, $value);)*
+            hm
+        }
+    }
 }
