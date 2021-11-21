94e8fc04b917980273f63d93ae77b82f03fddc5d
hehaoqian <39371646+hehaoqian@users.noreply.github.com>
2021-11-21 11:32:00 +0800

    semi-structured-logs done
    Signed-off-by: hehaoqian <39371646+hehaoqian@users.noreply.github.com>

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,30 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 /// various log levels
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug,
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+        LogLevel::Debug => debug(message),
+    }
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    format!("[INFO]: {}", message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    format!("[WARNING]: {}", message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    format!("[ERROR]: {}", message)
+}
+
+pub fn debug(message: &str) -> String {
+    format!("[DEBUG]: {}", message)
 }
